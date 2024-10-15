f13443d3df7b43872447de1f9f086d8f52c693e2
Ygor Leão <ygor__leao@hotmail.com>
2024-10-15 19:55:03 +0000

    Feita a endentação na camada intermediate de todos os models

--- models/intermediate/int_erp__tipo_cartao.sql
+++ models/intermediate/int_erp__tipo_cartao.sql
@@ -1,14 +1,14 @@
-with rename as (
-    select 
-     PK_ID_CARTAO_CLIENTE 
-    ,CARTAO_CLIENTE_TIPO_CARTAO 
-    ,CARTAO_CLIENTE_NUMERO 
-    ,CARTAO_CLIENTE_MES_EXPIRA 
-    ,CARTAO_CLIENTE_ANO_EXPIRA 
-    ,CARTAO_CLIENTE_MES_EXPIRA ||'/'||CARTAO_CLIENTE_ANO_EXPIRA  as "CARTAO_CLIENTE_DATA_EXPIRA"
-    ,CARTAO_CLIENTE_DATA_ATT
-    from {{ref("stg_erp__cartao_credito_cliente")}}
+with 
+    rename as (
+        select 
+             PK_ID_CARTAO_CLIENTE 
+            ,CARTAO_CLIENTE_TIPO_CARTAO 
+            ,CARTAO_CLIENTE_NUMERO 
+            ,CARTAO_CLIENTE_MES_EXPIRA 
+            ,CARTAO_CLIENTE_ANO_EXPIRA 
+            ,CARTAO_CLIENTE_MES_EXPIRA ||'/'||CARTAO_CLIENTE_ANO_EXPIRA  as "CARTAO_CLIENTE_DATA_EXPIRA"
+            ,CARTAO_CLIENTE_DATA_ATT
+        from {{ref("stg_erp__cartao_credito_cliente")}}
 )
-
 
 select * from rename
