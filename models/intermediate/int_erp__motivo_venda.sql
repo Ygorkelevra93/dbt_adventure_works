eef429b649aa8611da96017d7dcf42b7b98e5b20
Ygor Leão <ygor__leao@hotmail.com>
2024-10-16 14:17:30 +0000

    arrumado o status vazio para o relacionamento do motivo da ordem de venda

--- models/intermediate/int_erp__motivo_venda.sql
+++ models/intermediate/int_erp__motivo_venda.sql
@@ -1,5 +1,10 @@
 with 
-    relacaoVenda as  (
+    ordemVenda as(
+        select 
+            distinct pk_ordem_de_venda_ID
+        from {{ref("stg_erp__vendas_cabecalho")}}
+    ) 
+    ,relacaoVenda as  (
         select *
         from {{ref("stg_erp__relacao_venda_motivovenda")}}
     )
@@ -10,10 +15,11 @@ with
     
     ,joined as (
         select 
-             relacaoVenda.PK_ID_ORDEM_DE_VENDA  as  PK_ID_ORDEM_DE_VENDA
-            ,motivoVenda.MOTIVO_VENDA_NOME as MOTIVO_VENDA_NOME
-            ,motivoVenda.MOTIVO_VENDA_TIPO as MOTIVO_VENDA_TIPO
-        from relacaoVenda 
+             ordemVenda.pk_ordem_de_venda_ID  as  PK_ID_ORDEM_DE_VENDA
+            ,coalesce(motivoVenda.MOTIVO_VENDA_NOME,'Vazio') as MOTIVO_VENDA_NOME
+            ,coalesce(motivoVenda.MOTIVO_VENDA_TIPO,'Vazio') as MOTIVO_VENDA_TIPO
+        from ordemVenda 
+        left join relacaoVenda on relacaoVenda.pk_id_ordem_de_venda = ordemVenda.pk_ordem_de_venda_ID
         left join motivoVenda on motivoVenda.pk_id_motivo_venda = relacaoVenda.fk_id_temp_motivo_venda
     )
 select * from joined    
